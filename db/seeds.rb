e405c279c239ccac2fba859e5d2d0e00d1d6f9be
hassanssaade <hassanssaade@gmail.com>
2020-08-02 17:48:03 -0300

    Added CRUD operations and faker article generator"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(content: Faker::ChuckNorris.fact, title: Faker::Name.name_with_middle)
+end
